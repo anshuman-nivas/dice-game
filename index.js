07103f9dcabe55e04b20fe2b0b5f4aaf3036e429
Anshuman Nivas <125132858+anshuman-nivas@users.noreply.github.com>
2023-08-06 13:04:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNo1 = Math.floor(Math.random() * 6) + 1;
-var randomSrc = "images/dice" + "randomNo1" + ".png";
+var randomSrc = "images/dice" + randomNo1 + ".png";
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomSrc);
 
 var randomNo2 = Math.floor(Math.random() * 6) + 1;
-var randomSrc2 = "images/dice" + "randomNo2" + ".png";
+var randomSrc2 = "images/dice" + randomNo2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomSrc2);
